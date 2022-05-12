bcb257644958f1105c49b74d6e2c7c626bcbea48
Rafael <74336014+RCordeiroAlmeida@users.noreply.github.com>
2022-05-12 17:49:35 -0300

    Create 1067.py

---
+++ Beecrowd/1067.py
@@ -0,0 +1,16 @@
+x = int(input())
+i = 1
+while i <= x:
+    if i % 2 != 0:
+        print(i)
+    i = i + 1 
+
+'''
+i = 0
+x = int(input())
+
+if 1 <= x <= 1001:
+    for x in range(1,int(x)):
+        if x%2 != 0:
+            print(x)
+'''
