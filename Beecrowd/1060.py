bc679ba34d1f564164574ad0d83b486146d84092
Rafael <74336014+RCordeiroAlmeida@users.noreply.github.com>
2022-05-12 14:18:33 -0300

    Create 1060.py

---
+++ Beecrowd/1060.py
@@ -0,0 +1,16 @@
+y = 0
+for x in range(1,7):
+    a = float(input())
+    if a > 0:
+        y = y + 1
+print('{} valores positivos'.format(y))
+
+''' 
+i = 0
+x = input().split()
+lista = [float(n) for n in x]
+for n in lista:
+    if n > 0:
+        i+=1
+print(i)
+'''
